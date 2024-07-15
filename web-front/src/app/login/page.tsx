417ce35a74cc0da92a8fcebacf3239f6e0759096
wychoi <soc09072@gmail.com>
2024-07-16 00:31:54 +0900

    이미지 추가 및 색상 팔레트 추가

--- web-front/src/app/login/page.tsx
+++ web-front/src/app/login/page.tsx
@@ -1,8 +1,27 @@
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
+
 export default function LoginPage() {
     return (
-      <div>
-          LoginPage
+      <div className={styles.login_page}>
+          <h1>
+            Rainit
+          </h1>
+          <div>
+            <div className={styles.auth_title}>
+                로그인
+            </div>
+            <div>
+                <input type="text" placeholder="아이디"/>
+            </div>
+            <div>
+                <input type="password" placeholder="비밀번호"/>
+            </div>
+            
+          </div>
+          <div className={styles.login_button}>
+            
+            로그인
+          </div>
       </div>
     );
   }
---
+++ web-front/src/lib/styles/palette.js
@@ -0,0 +1,10 @@
+// const PALETTE = (function () {
+//     return {
+//       MAIN_GREEN: '#4BBD5D',
+//     }
+//   }())
+  
+//   export {
+//     PALETTE,
+//   }
+  
